5143214472a808ba6b4f69420385b42b6692559d
Yu Sato <ym.74.mn@gmail.com>
2022-02-06 21:38:04 +0900

    サーバからデータ取得

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { fetchImages } from "./api";
+
 function Header() {
   return (
     <header className="hero is-dark is-bold">
@@ -45,21 +48,12 @@ function Gallery(props) {
 }
 
 function Main() {
-  const urls = null;
-  // const urls = [
-  //   "https://images.dog.ceo/breeds/shiba/shiba-11.jpg",
-  //   "https://images.dog.ceo/breeds/shiba/shiba-12.jpg",
-  //   "https://images.dog.ceo/breeds/shiba/shiba-14.jpg",
-  //   "https://images.dog.ceo/breeds/shiba/shiba-17.jpg",
-  //   "https://images.dog.ceo/breeds/shiba/shiba-2.jpg",
-  //   "https://images.dog.ceo/breeds/shiba/shiba-3i.jpg",
-  //   "https://images.dog.ceo/breeds/shiba/shiba-4.jpg",
-  //   "https://images.dog.ceo/breeds/shiba/shiba-5.jpg",
-  //   "https://images.dog.ceo/breeds/shiba/shiba-6.jpg",
-  //   "https://images.dog.ceo/breeds/shiba/shiba-7.jpg",
-  //   "https://images.dog.ceo/breeds/shiba/shiba-8.jpg",
-  //   "https://images.dog.ceo/breeds/shiba/shiba-9.jpg",
-  // ];
+  const [urls, setUrls] = useState(null);
+  useEffect(() => {
+    fetchImages("shiba").then((urls) => {
+      setUrls(urls);
+    });
+  }, []);
   return (
     <main>
       <section className="section">
